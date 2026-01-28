c241bd35ba824f4b7436867a530e6a142e0dbd0a
Ritwik Gupta <ritwikgupta06@gmail.com>
2026-01-28 16:11:03 +0530

    Update index.js
    fixed a small logic error in the js file and changed the reset all function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let currHomeScore = document.getElementById("home-score")
-let currAwayScore = document.getElementById("away-score")
+let currAwayScore = document.getElementById("guest-score")
 
 function addH1() {
     currHomeScore.textContent = Number(currHomeScore.textContent) + 1
@@ -16,17 +16,18 @@ function addH3(){
 
 
 function addA1(){
-    currAwayScore.textContent = Number(currHomeScore.textContent) + 1
+    currAwayScore.textContent = Number(currAwayScore.textContent) + 1
 }
 
 function addA2(){
-    currAwayScore.textContent = Number(currHomeScore.textContent) + 2
+    currAwayScore.textContent = Number(currAwayScore.textContent) + 2
 }
 
 function addA3(){
-    currAwayScore.textContent = Number(currHomeScore.textContent) + 3
+    currAwayScore.textContent = Number(currAwayScore.textContent) + 3
 }
 
 function resetAll(){
-    currAwayScore.textContent = currHomeScore.textContent = Number(0)
+    currAwayScore.textContent = 0
+    currHomeScore.textContent = 0
 }
